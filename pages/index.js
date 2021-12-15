b42f7e8048da9b8bbede32b3dcc9f42262bd3700
hamazi163 <hamadayuusuke@au.com>
2021-12-15 12:29:26 +0900

    test

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 /** @format */
 
-// import * as React from "react";
+import * as React from "react";
 import Head from "next/head";
 import Image from "next/image";
 import { Container, Paper } from "@mui/material";
