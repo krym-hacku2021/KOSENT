63274d05b24d65dbe6b37e91e3c67976ef40191c
hamazi163 <hamadayuusuke@au.com>
2021-12-16 11:46:10 +0900

    ?

--- pages/text.js
+++ pages/text.js
@@ -1,9 +1,9 @@
 // import Link from "next/link";
 import Image from "next/image";
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import EXAppBar from "./EXAppBar";
 export default function question(props){
-    const[text,setText]=useState("")
+    // const[text,setText]=useState("")
     // function handlonChange(event){
     //     setText(event.target.value);
     // }
@@ -12,9 +12,9 @@ export default function question(props){
     //    event.preventDefault(); 
     // }
 
-    const update=function(){
-        props.update(text);
-    }
+    // const update=function(){
+    //     props.update(text);
+    // }
 
   return (
   <div>
@@ -41,7 +41,7 @@ export default function question(props){
         </div>
         <div className="b">
         <textarea className="txt" placeholder="3文字以上入力してください" 
-        onChange={event => setText(event.target.value)}
+        // onChange={event => setText(event.target.value)}
         /><br></br>
         <p className="sita">あなたにおすすめのタグ<br></br>
         <button className="kadomaru">#広島商船</button>
